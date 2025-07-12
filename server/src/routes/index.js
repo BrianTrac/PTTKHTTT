f6e6bc58f1c6d5385f8f49883c1cba37c7142526
BrianTrac <dangphuchung321@gmail.com>
2025-07-12 07:50:07 +0700

    feat: add endpoint for XemDanhSachDangKy UI and TraCuuThongTinChungChi UI

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,17 +3,19 @@ const router = express.Router();
 
 // Import routes
 const phieuDangKyRoutes = require("./phieuDangKy");
+const thiSinhRoutes = require("./thiSinh");
 
 // Use routes
 router.use("/phieudangky", phieuDangKyRoutes);
+router.use("/thisinhs", thiSinhRoutes);
 
 // Health
 router.get("/health", (req, res) => {
-	res.status(200).json({
-		success: true,
-		message: "API đang hoạt động",
-		timestamp: new Date().toISOString(),
-	});
+  res.status(200).json({
+    success: true,
+    message: "API đang hoạt động",
+    timestamp: new Date().toISOString(),
+  });
 });
 
 module.exports = router;
